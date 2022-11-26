f175f1e7707d33de2b6b4add987b552240ff31b1
mutian <bhfqlz@qq.com>
2022-11-26 19:48:02 +0800

    version 1.0.4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'muyomu/log4p',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f69297da578314756f3c653fff6a67fb11ce7e10',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'muyomu/config' => array(
+            'pretty_version' => 'v1.0.7',
+            'version' => '1.0.7.0',
+            'reference' => '526ec61c34e98b23fc90b90fc49852394ecb4626',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../muyomu/config',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'muyomu/log4p' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f69297da578314756f3c653fff6a67fb11ce7e10',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
